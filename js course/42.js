3d946f94a04fc560ab25886a5bd4b8c68b01af22
Nadia <nadiadunaeva@gmail.com>
2022-11-10 17:59:23 +0100

    "new tasks added"

---
+++ js course/42.js
@@ -0,0 +1,4 @@
+// Setup
+const myArray = [18, 64, 99];
+// Only change code below this line
+myArray[0]=45;
---
+++ js course/48.js
@@ -0,0 +1,1 @@
+const myList = [["Milk", 3.35] ,  ["Bread", 4.50],["Sushi", 7.90],["Cacao", 1.50],["Baking powder", 0.50]];
